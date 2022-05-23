a8fef834d579fff4940b4584d879276e332b60a7
syukoGit <syukodev@outlook.com>
2022-05-23 11:09:58 +0200

    Add onClickHandler and onClickAsync
    Signed-off-by: syukoGit <syukodev@outlook.com>

--- app/src/main/java/fr/android/calculator/MainActivity.java
+++ app/src/main/java/fr/android/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package fr.android.calculator;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,13 +20,15 @@ import java.util.Arrays;
 @SuppressWarnings("ClassIndependentOfModule")
 public class MainActivity extends AppCompatActivity {
 
-    private final DecimalFormat resultFormat = new DecimalFormat("##.##");
+    protected final DecimalFormat resultFormat = new DecimalFormat("##.##");
 
-    public Button btnEquals = null;
+    protected String operation = "";
 
-    public String operation = "";
+    protected String resultStr = "";
 
-    public String resultStr = "";
+    private Button btnEquals = null;
+
+    private Handler handler;
 
     public boolean isOperand(String value) {
         String[] operands = { this.getResources().getString(R.string.textBtnDivide),
@@ -35,6 +40,134 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        // this.onClickProcedural(view);
+        // this.onClickHandler(view);
+        this.onClickAsync(view);
+    }
+
+    /**
+     * @param operationToEvaluate operation to be evaluated
+     *
+     * @return result of the operation
+     *
+     * @throws IllegalArgumentException throw if operation is empty or ends in an operation
+     * @throws ArithmeticException throw if the operation is invalid
+     * @throws NumberFormatException throw if the result can't be parsed to a double
+     */
+    protected double evaluateOperation(@NotNull String operationToEvaluate) {
+        String lastValue = operationToEvaluate.isEmpty()
+                           ? ""
+                           : operationToEvaluate.substring(operationToEvaluate.length() - 1);
+
+        if (this.isOperand(lastValue) || operationToEvaluate.isEmpty()) {
+            throw new IllegalArgumentException("Operation is invalid");
+        }
+
+        Context context = Context.enter();
+        context.setOptimizationLevel(-1);
+        Scriptable scope = context.initStandardObjects();
+        String result = context.evaluateString(scope, operationToEvaluate, "<cmd>", 1, null).toString();
+
+        if (result.contains("Infinity")) {
+            throw new ArithmeticException("Unable to divide by zero");
+        } else {
+            return Double.parseDouble(result);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+        this.handler = new Handler();
+
+        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 4);
+        layoutParam.setMargins(5, 12, 5, 12);
+
+        this.btnEquals = new Button(this);
+        this.btnEquals.setText(R.string.textBtnEquals);
+        this.btnEquals.setTextAppearance(R.style.TextAppearance_AppCompat_Display1);
+        this.btnEquals.setOnClickListener(this::onClick);
+        this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.gray, null));
+        this.btnEquals.setTextColor(this.getResources().getColor(R.color.black, null));
+        this.btnEquals.setEnabled(false);
+
+        LinearLayout linearLayout = this.findViewById(R.id.layoutButtons);
+        linearLayout.addView(this.btnEquals, layoutParam);
+    }
+
+    void setEnabledBtnEquals(boolean enabled) {
+        if (enabled) {
+            this.btnEquals.setEnabled(true);
+            this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.purple_200, null));
+        } else {
+            this.btnEquals.setEnabled(false);
+            this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.gray, null));
+        }
+    }
+
+    private void onClickAsync(View view) {
+        ActivityTask task = new ActivityTask(view);
+        task.execute();
+    }
+
+    private void onClickHandler(View view) {
+        if (view instanceof Button) {
+            Button viewAsButton = (Button) view;
+            String text = viewAsButton.getText().toString();
+            String lastValue = this.operation.isEmpty()
+                               ? ""
+                               : this.operation.substring(this.operation.length() - 1);
+
+            TextView editTextOperation = this.findViewById(R.id.editTextOperation);
+            TextView editTextResult = this.findViewById(R.id.editTextResult);
+
+            String equalValue = this.getResources().getString(R.string.textBtnEquals);
+            if (text.equals(equalValue)) {
+                try {
+                    double result = this.evaluateOperation(this.operation);
+
+                    this.handler.post(() -> this.resultStr = this.resultFormat.format(result));
+                    this.handler.post(() -> this.operation = "");
+                } catch (IllegalArgumentException | ArithmeticException ignored) {
+                    this.handler.post(() -> this.resultStr = "");
+                    this.handler.post(() -> editTextResult.setBackground(this.getResources()
+                                                                             .getDrawable(R.drawable.textview_red_border, null)));
+                    this.handler.post(() -> editTextResult.setText(R.string.string_error));
+
+                    return;
+                }
+            } else {
+                boolean isOperand = this.isOperand(text);
+
+                if (isOperand) {
+                    boolean lastIsOperand = this.isOperand(lastValue);
+
+                    boolean operandIsTheFirstValue = this.operation.isEmpty();
+
+                    if (lastIsOperand) {
+                        this.handler.post(() -> this.operation = this.operation.substring(0,
+                                                                                          this.operation.length() - 1));
+                    }
+
+                    if (operandIsTheFirstValue) {
+                        return;
+                    }
+                }
+
+                this.handler.post(() -> this.operation += text);
+            }
+
+            this.handler.post(() -> this.setEnabledBtnEquals(!this.operation.isEmpty() && !this.isOperand(text)));
+
+            this.handler.post(() -> editTextResult.setBackground(this.getResources()
+                                                                     .getDrawable(R.drawable.textview_purple_border, null)));
+            this.handler.post(() -> editTextOperation.setText(this.operation));
+            this.handler.post(() -> editTextResult.setText(this.resultStr));
+        }
+    }
+
+    private void onClickProcedural(View view) {
         if (view instanceof Button) {
             Button viewAsButton = (Button) view;
             String text = viewAsButton.getText().toString();
@@ -87,63 +220,86 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_main);
+    @SuppressLint("StaticFieldLeak")
+    private class ActivityTask extends AsyncTask<Void, Void, String> {
 
-        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 4);
-        layoutParam.setMargins(5, 12, 5, 12);
+        private final View view;
 
-        this.btnEquals = new Button(this);
-        this.btnEquals.setText(R.string.textBtnEquals);
-        this.btnEquals.setTextAppearance(R.style.TextAppearance_AppCompat_Display1);
-        this.btnEquals.setOnClickListener(this::onClick);
-        this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.gray, null));
-        this.btnEquals.setTextColor(this.getResources().getColor(R.color.black, null));
-        this.btnEquals.setEnabled(false);
+        private boolean error = false;
 
-        LinearLayout linearLayout = this.findViewById(R.id.layoutButtons);
-        linearLayout.addView(this.btnEquals, layoutParam);
-    }
-
-    /**
-     * @param operationToEvaluate operation to be evaluated
-     *
-     * @return result of the operation
-     *
-     * @throws IllegalArgumentException throw if operation is empty or ends in an operation
-     * @throws ArithmeticException throw if the operation is invalid
-     * @throws NumberFormatException throw if the result can't be parsed to a double
-     */
-    private double evaluateOperation(@NotNull String operationToEvaluate) {
-        String lastValue = operationToEvaluate.isEmpty()
-                           ? ""
-                           : operationToEvaluate.substring(operationToEvaluate.length() - 1);
-
-        if (this.isOperand(lastValue) || operationToEvaluate.isEmpty()) {
-            throw new IllegalArgumentException("Operation is invalid");
+        ActivityTask(View view) {
+            this.view = view;
         }
 
-        Context context = Context.enter();
-        context.setOptimizationLevel(-1);
-        Scriptable scope = context.initStandardObjects();
-        String result = context.evaluateString(scope, operationToEvaluate, "<cmd>", 1, null).toString();
+        @Override
+        protected @NotNull String doInBackground(Void... voids) {
+            this.error = false;
 
-        if (result.contains("Infinity")) {
-            throw new ArithmeticException("Unable to divide by zero");
-        } else {
-            return Double.parseDouble(result);
+            if (this.view instanceof Button) {
+                Button viewAsButton = (Button) this.view;
+                String text = viewAsButton.getText().toString();
+                String lastValue = MainActivity.this.operation.isEmpty()
+                                   ? ""
+                                   : MainActivity.this.operation.substring(MainActivity.this.operation.length() - 1);
+
+                String equalValue = MainActivity.this.getResources().getString(R.string.textBtnEquals);
+                if (text.equals(equalValue)) {
+                    try {
+                        double result = MainActivity.this.evaluateOperation(MainActivity.this.operation);
+
+                        MainActivity.this.resultStr = MainActivity.this.resultFormat.format(result);
+                        MainActivity.this.operation = "";
+                    } catch (IllegalArgumentException | ArithmeticException ignored) {
+                        this.error = true;
+                        return "";
+                    }
+                } else {
+                    boolean isOperand = MainActivity.this.isOperand(text);
+
+                    if (isOperand) {
+                        boolean lastIsOperand = MainActivity.this.isOperand(lastValue);
+
+                        boolean operandIsTheFirstValue = MainActivity.this.operation.isEmpty();
+
+                        if (lastIsOperand) {
+                            MainActivity.this.operation = MainActivity.this.operation.substring(0,
+                                                                                                MainActivity.this.operation.length() -
+                                                                                                1);
+                        }
+
+                        if (operandIsTheFirstValue) {
+                            return "";
+                        }
+                    }
+
+                    MainActivity.this.operation += text;
+                }
+
+                return text;
+            }
+
+            return "";
         }
-    }
 
-    private void setEnabledBtnEquals(boolean enabled) {
-        if (enabled) {
-            this.btnEquals.setEnabled(true);
-            this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.purple_200, null));
-        } else {
-            this.btnEquals.setEnabled(false);
-            this.btnEquals.setBackgroundColor(this.getResources().getColor(R.color.gray, null));
+        @Override
+        protected void onPostExecute(String text) {
+            TextView editTextOperation = MainActivity.this.findViewById(R.id.editTextOperation);
+            TextView editTextResult = MainActivity.this.findViewById(R.id.editTextResult);
+
+            if (this.error) {
+                MainActivity.this.resultStr = "";
+                editTextResult.setBackground(MainActivity.this.getResources()
+                                                              .getDrawable(R.drawable.textview_red_border, null));
+                editTextResult.setText(R.string.string_error);
+            } else {
+                MainActivity.this.setEnabledBtnEquals(
+                        !MainActivity.this.operation.isEmpty() && !MainActivity.this.isOperand(text));
+
+                editTextResult.setBackground(MainActivity.this.getResources()
+                                                              .getDrawable(R.drawable.textview_purple_border, null));
+                editTextOperation.setText(MainActivity.this.operation);
+                editTextResult.setText(MainActivity.this.resultStr);
+            }
         }
     }
 }
